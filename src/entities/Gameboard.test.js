9391656f3aac2eb591b358e8c5edc313dc9d7a09
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-04-12 18:53:07 -0300

    Add ship is placed tests

--- src/entities/Gameboard.test.js
+++ src/entities/Gameboard.test.js
@@ -32,3 +32,22 @@ test("Gameboard size", () => {
     expect(board[board.length - 1].length).toBe(size.width);
   });
 });
+
+describe("Gameboard place ship", () => {
+  test("Gameboard ship is placed", () => {
+    const gameboard = new Gameboard(10);
+    gameboard.placeShip(2, 2, 3, true);
+
+    // ship
+    expect(gameboard.getCell(2, 2).ship).toBeDefined();
+    expect(gameboard.getCell(2, 3).ship).toBeDefined();
+    expect(gameboard.getCell(2, 4).ship).toBeDefined();
+    // ends
+    expect(gameboard.getCell(2, 1).ship).toBeNull();
+    expect(gameboard.getCell(2, 5).ship).toBeNull();
+
+    //sides
+    expect(gameboard.getCell(1, 3).ship).toBeNull();
+    expect(gameboard.getCell(3, 3).ship).toBeNull();
+  });
+});

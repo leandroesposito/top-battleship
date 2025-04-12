26efb1b4575782b9de18bad35cb9ede96d1b2631
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-04-12 18:53:41 -0300

    Add placeShip, getcell and getSanitizedShip methods

--- src/entities/Gameboard.js
+++ src/entities/Gameboard.js
@@ -1,3 +1,5 @@
+import Ship from "./Ship.js";
+
 export default class Gameboard {
   #width;
   #height;
@@ -13,5 +15,34 @@ export default class Gameboard {
 
   getBoard() {
     return JSON.parse(JSON.stringify(this.#board));
+  }
+
+  placeShip(origX, origY, size, isHorizontal) {
+    const ship = new Ship(size);
+
+    if (isHorizontal) {
+      for (let x = origX; x < origX + size; x++) {
+        this.#board[origY][x].ship = ship;
+      }
+    } else {
+      for (let y = origY; y < origY + size; y++) {
+        this.#board[y][origX].ship = ship;
+      }
+    }
+  }
+
+  getCell(x, y) {
+    const cell = this.#board[y][x];
+
+    return {
+      ship: cell.ship ? this.#getSanitizedShip(cell.ship) : null,
+      hasBeenHit: cell.hasBeenHit,
+    };
+  }
+
+  #getSanitizedShip(ship) {
+    return {
+      isSunk: ship.isSunk(),
+    };
   }
 }

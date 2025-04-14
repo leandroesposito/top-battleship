13fd292ed1613a4e09e623cf0070679e79080905
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-04-14 20:33:14 -0300

    Create ComputerPlayer class

---
+++ src/entities/ComputerPlayer.js
@@ -0,0 +1,19 @@
+import Player from "./Player.js";
+
+export default class ComputerPlayer extends Player {
+  #attacksGameboard;
+  constructor(name, gameboard) {
+    super(name, gameboard);
+    this.#attacksGameboard = initializeAttackGameBoard(gameboard);
+  }
+
+  initializeAttackGameBoard(gameboard) {
+    const attackGameboard = new Set();
+    for (let i = 0; i < gameboard.getBoard().length; i++) {
+      for (let j = 0; j < gameboard.getBoard()[i].length; j++) {
+        attackGameboard.add({ x: i, y: j });
+      }
+    }
+    return attackGameboard;
+  }
+}

be6837ed7b03970ca9b7b744b2219c1895ba820a
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-04-15 16:11:03 -0300

    Change attacks initializer name and make static

--- src/entities/ComputerPlayer.js
+++ src/entities/ComputerPlayer.js
@@ -4,10 +4,10 @@ export default class ComputerPlayer extends Player {
   #attacksGameboard;
   constructor(name, gameboard) {
     super(name, gameboard);
-    this.#attacksGameboard = initializeAttackGameBoard(gameboard);
+    this.#attacksGameboard = ComputerPlayer.#initializeAttacksList(gameboard);
   }
 
-  initializeAttackGameBoard(gameboard) {
+  static #initializeAttacksList(gameboard) {
     const attackGameboard = new Set();
     for (let i = 0; i < gameboard.getBoard().length; i++) {
       for (let j = 0; j < gameboard.getBoard()[i].length; j++) {

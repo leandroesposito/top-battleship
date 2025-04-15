450c880630441ac6c3547538313cc24f9e1ebd47
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-04-15 18:12:14 -0300

    Create Game class

---
+++ src/entities/Game.js
@@ -0,0 +1,32 @@
+export default class Game {
+  constructor(player1, player2) {
+    this.player1 = player1;
+    this.player2 = player2;
+    this.currentPlayer = player1;
+  }
+
+  getCurrentPlayer() {
+    return this.currentPlayer;
+  }
+
+  attack(x, y) {
+    if (this.currentPlayer.allShipsSunk()) {
+      return false;
+    }
+
+    const attackResult = this.currentPlayer.receiveAttack(x, y);
+    if (attackResult && !this.currentPlayer.allShipsSunk()) {
+      this.#switchPlayer(attackResult);
+    }
+
+    return attackResult;
+  }
+
+  #switchPlayer() {
+    if (this.currentPlayer === this.player1) {
+      this.currentPlayer = this.player2;
+    } else {
+      this.currentPlayer = this.player1;
+    }
+  }
+}

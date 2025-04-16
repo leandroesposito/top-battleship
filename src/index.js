408e38ec1f52cfbaff71734185339693b34500b6
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-04-16 15:57:07 -0300

    Load setup form on start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./style.css";
+import "./setup.css";
+import generateSetupForm from "./setup-form-generator.js";
+
+(function init() {
+  function loadContent(functionGenerator) {
+    const main = document.querySelector("main");
+    main.innerHTML = "";
+    main.appendChild(functionGenerator());
+  }
+
+  loadContent(generateSetupForm);
+})();

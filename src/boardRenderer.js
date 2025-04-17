bfac4d131880bfc2152b1f0bbb080bd55b1e1fe9
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-04-17 19:43:51 -0300

    Create boardRenerer method

---
+++ src/boardRenderer.js
@@ -0,0 +1,25 @@
+import createElement from "./htmlGenerators.js";
+
+export default function generateBoard(gameboard) {
+  const boardElement = createElement("div", "gameboard");
+  const board = gameboard.getBoard();
+
+  for (let y = 0; y < board.length; y++) {
+    for (let x = 0; x < board[y].length; x++) {
+      const cellData = board[y][x];
+      const cell = createElement("div", "cell");
+      cell.dataset.x = x;
+      cell.dataset.y = y;
+      if (cellData.ship === null) {
+        cell.classList.add("ship");
+      }
+      if (cellData.hastBeenHit === true) {
+        cell.classList.add("hit");
+      }
+
+      boardElement.appendChild(cell);
+    }
+  }
+
+  return boardElement;
+}

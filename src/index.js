ef7e4a7250a54f956e30d4f5ac8f50af54bc708b
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-04-18 21:03:42 -0300

    Add game init and turn renderer test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import "./style.css";
 import "./setup.css";
-import generateSetupForm from "./setup-form-generator.js";
+import "./game.css";
+import "./gameboard.css";
+
+import Player from "./entities/Player.js";
+import GameBoard from "./entities/Gameboard.js";
+
+import initGame from "./gameController.js";
 
 (function init() {
   function loadContent(functionGenerator) {
@@ -9,5 +15,26 @@ import generateSetupForm from "./setup-form-generator.js";
     main.appendChild(functionGenerator());
   }
 
-  loadContent(generateSetupForm);
+  const g1 = new GameBoard(10);
+  const g2 = new GameBoard(10);
+
+  g1.placeShip(0, 0, 1, true);
+  g1.placeShip(4, 5, 3, true);
+
+  g1.receiveAttack(0, 0);
+  g1.receiveAttack(4, 5);
+  g1.receiveAttack(6, 5);
+
+  g2.placeShip(2, 2, 1, true);
+  g2.placeShip(7, 5, 3, false);
+
+  console.log(g2.receiveAttack(0, 0));
+  console.log(g2.receiveAttack(2, 2));
+  console.log(g2.receiveAttack(7, 6));
+
+  const player1 = new Player("Player 1", g1);
+  const player2 = new Player("Player 2", g2);
+
+  const game = initGame(player1, player2);
+  // loadContent(generateSetupForm);
 })();

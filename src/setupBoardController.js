1de6579ce206e495428de7edf89c14d1a0f796cd
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-04-21 20:33:25 -0300

    Extract showing error to function

--- src/setupBoardController.js
+++ src/setupBoardController.js
@@ -29,37 +29,47 @@ export default function initSetupBoard(playerName, boardSize) {
   function validateInput(input, showError = true) {
     const fieldset = input.closest("fieldset");
     const inputs = [input];
+    let errorMessage = "";
 
     for (let i = 0; i < inputs.length; i++) {
       const input = inputs[i];
       let isValid = true;
 
       if (input.validity.valueMissing) {
-        errorMessage.textContent = "This field is required.";
+        errorMessage = "This field is required.";
         isValid = false;
       } else if (
         input.validity.rangeOverflow ||
         input.validity.rangeUnderflow
       ) {
-        errorMessage.textContent = `Value must be between 1 and ${boardSize}.`;
+        errorMessage = `Value must be between 1 and ${boardSize}.`;
         isValid = false;
       } else if (input.validity.badInput) {
-        errorMessage.textContent = "Value must be an integer number.";
+        errorMessage = "Value must be an integer number.";
         isValid = false;
       }
 
       if (!isValid) {
-        input.classList.add("invalid");
+        if (showError) {
+          setFieldsetError(fieldset, errorMessage);
+          input.classList.add("invalid");
+        }
         return false;
       }
     }
 
     if (fieldsetIsValid(fieldset)) {
-      errorMessage.textContent = "";
+      if (showError) {
+        setFieldsetError(fieldset, "");
+      }
       return true;
     }
-
     return false;
+  }
+
+  function setFieldsetError(fieldset, errorMessage) {
+    const errorElement = fieldset.querySelector(".error-message");
+    errorElement.textContent = errorMessage;
   }
 
   function fieldsetIsValid(fieldset) {

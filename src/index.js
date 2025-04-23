71410008ad2618a143c10325fc9308ec7f694830
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-04-23 18:21:52 -0300

    Remove unused test code

--- src/index.js
+++ src/index.js
@@ -4,23 +4,6 @@ import "./game.css";
 import "./gameboard.css";
 import "./setupBoard.css";
 
-import Player from "./entities/Player.js";
-import GameBoard from "./entities/Gameboard.js";
-
-import initGame from "./gameController.js";
-
-import generateSetupBoardForm from "./setupBoardFormRenderer.js";
-
-import initSetupBoard from "./setupBoardController.js";
-
 import initSetup from "./setupController.js";
 
-(function init() {
-  function loadContent(functionGenerator) {
-    const main = document.querySelector("main");
-    main.innerHTML = "";
-    main.appendChild(functionGenerator());
-  }
-
-  initSetup();
-})();
+initSetup();

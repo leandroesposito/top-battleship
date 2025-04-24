37586ce1ef4987a0b4c921c194a8bcb314d39be9
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-04-24 18:06:13 -0300

    Add better attack position generator
    - Instead of generating a random position every time
    - Register successful attacks and search valid positions next to those attacks
    - Use a stack to track successful attacks and pop elements
    when all positions around are checked as invalid

--- src/entities/ComputerPlayer.js
+++ src/entities/ComputerPlayer.js
@@ -2,9 +2,11 @@ import Player from "./Player.js";
 
 export default class ComputerPlayer extends Player {
   #attacksGameboard;
+  #hitsStack;
   constructor(name, gameboard) {
     super(name, gameboard);
     this.#attacksGameboard = ComputerPlayer.#initializeAttacksList(gameboard);
+    this.#hitsStack = [];
   }
 
   static #initializeAttacksList(gameboard) {
@@ -25,6 +27,20 @@ export default class ComputerPlayer extends Player {
     this.#attacksGameboard.delete(JSON.stringify(coords));
   }
 
+  getAttack() {
+    while (this.#hitsStack.length > 0) {
+      const lastHit = this.#hitsStack[this.#hitsStack.length - 1];
+      const newPosition = this.getValidNeighbor(lastHit);
+      if (newPosition) {
+        this.#attacksGameboardDelete(newPosition);
+        return newPosition;
+      }
+      this.#hitsStack.pop();
+    }
+
+    return this.getRandomAttack();
+  }
+
   getRandomAttack() {
     if (this.#attacksGameboard.size === 0) {
       throw new Error("No spaces left to attack");
@@ -36,5 +52,27 @@ export default class ComputerPlayer extends Player {
     this.#attacksGameboard.delete(randomAttack);
 
     return JSON.parse(randomAttack);
+  }
+
+  registerHit({ x, y }) {
+    this.#hitsStack.push({ x, y });
+  }
+
+  getValidNeighbor({ x, y }) {
+    const positions = [
+      { x, y: y - 1 }, // up
+      { x: x + 1, y }, // right
+      { x, y: y + 1 }, // down
+      { x: x - 1, y }, // left
+    ];
+
+    for (let i = 0; i < positions.length; i++) {
+      const position = positions[i];
+      if (this.#attacksGameboardHas(position)) {
+        return position;
+      }
+    }
+
+    return false;
   }
 }
